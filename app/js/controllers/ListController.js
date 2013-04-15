71ce3f960c9f87a3262ff8d4a42e74cc4db37bd8
Olivier SCHNEIDER <76267@supinfo.com>
2013-04-15 14:38:20 +0200

    Add the new mission form

--- app/js/controllers/ListController.js
+++ app/js/controllers/ListController.js
@@ -1,7 +1,20 @@
 
 smurAngular.controller('ListController', 
-	function ListController($scope, Mission){
-		$scope.missions = Mission.query(function(){
-			console.log("done");
-		});
+	function ListController($scope, $http, Mission){
+		$scope.missions = Mission.query();
+
+		 $http.get("/resources/vehicles.json").success(function(data){
+		 	$scope.vehicles = data;
+		 });
+
+		 $http.get("/resources/responsibles.json").success(function(data){
+		 	$scope.responsibles = data;
+		 });
+
+		 $scope.add = function(){
+		 	console.log("Saving");
+		 	console.log($scope.vehicle);
+		 	console.log($scope.responsible);
+		 	console.log($scope.password);
+		 };
 	});
