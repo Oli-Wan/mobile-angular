0fec5bef68fe61c8535a0daf50730476a81b859e
Olivier SCHNEIDER <76267@supinfo.com>
2013-04-16 12:00:40 +0200

    Create a container for the mission description and create the first form of it

--- app/js/app.js
+++ app/js/app.js
@@ -5,7 +5,15 @@
 var smurAngular = angular.module('smurAngular', ['ngResource', '$strap.directives']);
 
 smurAngular.config(['$routeProvider', function($routeProvider) {
-    $routeProvider.when('/', {templateUrl: 'partials/missions.html', controller: 'MissionsController'});
-    $routeProvider.when('/mission/:missionId', {templateUrl: 'partials/mission.html', controller: 'MissionController'});
+    $routeProvider.when('/', 
+    	{
+    		templateUrl: 'partials/missions.html', 
+    		controller: 'MissionsController'
+    	});
+    $routeProvider.when('/mission/:missionId', 
+    	{
+    		templateUrl: 'partials/mission-container.html', 
+			controller: 'MissionController'
+    	});
     $routeProvider.otherwise({redirectTo: '/'});
   }]);
