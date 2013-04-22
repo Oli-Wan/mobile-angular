881ec11ba6ceb07add8bc1b7bbbdaf5166b9bf86
Olivier SCHNEIDER <76267@supinfo.com>
2013-04-22 14:07:55 +0200

    Add Event store service

--- app/js/controllers/EventController.js
+++ app/js/controllers/EventController.js
@@ -1,12 +1,14 @@
 
 smurAngular.controller("EventController", 
-	function EventController($scope, $http, $modal, $routeParams, $location, Mission){		
+	function EventController($scope, $http, $modal, $routeParams, $location, Mission, Event){		
 		Mission.getStore().then(function(store){
 			store.get(parseInt($routeParams.missionId), function(data) {
 				$scope.mission = data;
 				$scope.$apply();
 			});
 		});
+
+		
 
 		$scope.goToNewEvent = function() {
 			$location.url("/mission/"+$scope.mission.id+"/events/new");
---
+++ app/js/services/Event.js
@@ -0,0 +1,31 @@
+
+smurAngular.factory("Event", function Event($timeout){
+	var storeWrapper = {
+		ready:false,
+		setReady: function() {
+			this.ready = true;
+		},
+		getStore: function() {
+			return $timeout(waitForStore);
+		},
+		store: new IDBStore({
+			dbVersion: 1,
+			storeName: 'event',
+			keyPath: 'id',
+			autoIncrement: true,
+			onStoreReady: function() {
+				storeWrapper.setReady();
+			}
+		})
+	};
+
+	function waitForStore() {
+		if(storeWrapper.ready) {
+			return storeWrapper.store;
+		}
+		else {
+			return $timeout(waitForStore, 100);
+		}
+	}
+	return storeWrapper;
+});
