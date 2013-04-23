45ba00985a64cb8d799c599f118eb777a1a46650
Olivier SCHNEIDER <76267@supinfo.com>
2013-04-23 15:14:03 +0200

    Put new vehicles in store

--- app/js/controllers/NewVehicleController.js
+++ app/js/controllers/NewVehicleController.js
@@ -1,9 +1,16 @@
 
 smurAngular.controller("NewVehicleController", 
-	function NewVehicleController($scope, $http, $location, $routeParams, Mission) {
+	function NewVehicleController($scope, $http, $location, $routeParams, Mission, Vehicle, Utils) {
 		Mission.getStore().then(function(store){
 			store.get(parseInt($routeParams.missionId), function(data) {
 				$scope.mission = data;
+				$scope.$apply();
+			});
+		});
+
+		Vehicle.getStore().then(function(store) {
+			store.getAll(function(data) {
+				$scope.vehicles = data;
 				$scope.$apply();
 			});
 		});
@@ -16,16 +23,22 @@ smurAngular.controller("NewVehicleController",
 			});
 		});
 
-		$http.get('/resources/vehicles.json').success(function(data){
-			$scope.vehicles = data;
-		});
-
-
 		$scope.back = function() {
 			$location.url("/mission/"+$scope.mission.id).search({page: "vehicle"});
 		};
 
 		$scope.add = function() {
+			$scope.vehicle.store = "vehicle";
+			$scope.vehicle.time = Utils.getCurrentDateAndTime():
+			
+			if($scope.mission.vehicles === undefined)
+				$scope.mission.vehicles = [];
+			
+			$scope.mission.vehicles.push($scope.vehicle);
+
+			Mission.getStore().then(function(store){
+				store.put($scope.mission);
+			});
 			$scope.back();
 		};
 	});
--- app/js/controllers/VehicleController.js
+++ app/js/controllers/VehicleController.js
@@ -1,6 +1,6 @@
 
 smurAngular.controller("VehicleController", 
-	function VehicleController($scope, $routeParams, $location, Mission) {		
+	function VehicleController($scope, $routeParams, $location, Mission, Vehicle) {		
 		Mission.getStore().then(function(store){
 			store.get(parseInt($routeParams.missionId), function(data) {
 				$scope.mission = data;
@@ -10,5 +10,21 @@ smurAngular.controller("VehicleController",
 
 		$scope.goToNewVehicle = function() {
 			$location.url("/mission/"+$scope.mission.id+"/vehicles/new");
-		}
+		};
+
+		$scope.refreshVehicles = function() {
+			if($scope.mission.vehicles === undefined)
+				return;
+			
+			$scope.vehicles = [];
+			$scope.mission.vehicles.forEach(function(element, index, array) {
+				Vehicle.getStore().then(function(store){
+					store.get(parseInt(element.id), function(data){
+						data.time = element.time;
+						$scope.vehicles.push(data);
+						$scope.$apply();
+					});
+				});
+			});
+		};
 	});
