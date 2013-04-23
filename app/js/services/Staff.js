dd9955fe0b6fd2b916c85bb94451b09e18093afb
Olivier SCHNEIDER <76267@supinfo.com>
2013-04-23 15:27:49 +0200

    Remove console.log

--- app/js/services/Staff.js
+++ app/js/services/Staff.js
@@ -6,7 +6,6 @@ smurAngular.factory("Staff", function Staff($timeout){
 			this.ready = true;
 		},
 		getStore: function() {
-			console.log("Getting store");
 			return $timeout(waitForStore);
 		},
 		store: new IDBStore({
@@ -21,9 +20,7 @@ smurAngular.factory("Staff", function Staff($timeout){
 	};
 
 	function waitForStore() {
-		console.log("Wait for store");
 		if(storeWrapper.ready) {
-			console.log("Store : "+storeWrapper);
 			return storeWrapper.store;
 		}
 		else {
