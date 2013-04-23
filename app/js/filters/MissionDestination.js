24b5f53c0749129bf87d6c2f84cf6147564ec926
Olivier SCHNEIDER <76267@supinfo.com>
2013-04-23 16:43:25 +0200

    Add filter for the destination on mission page

---
+++ app/js/filters/MissionDestination.js
@@ -0,0 +1,11 @@
+smurAngular.filter('missionDestination', function(){
+	return function(events) {
+		if(events === undefined)
+			return;
+
+		for(var i = 0; i < events.length; i++) {
+			if(events[i].type == intervention)
+				return events[i].city;
+		}
+	};
+});
