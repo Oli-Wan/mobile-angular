4e4c84cf88998eab9250bca77ef7461802e1229c
Olivier SCHNEIDER <76267@supinfo.com>
2013-04-24 14:09:19 +0200

    Add zeros if needed

--- app/js/services/Utils.js
+++ app/js/services/Utils.js
@@ -3,9 +3,19 @@
  		getCurrentDateAndTime: function(){
  			var currentTime = new Date();
  			return {
- 				date: currentTime.getDate()+"/"+currentTime.getMonth()+"/"+currentTime.getFullYear(),
- 				time: currentTime.getHours()+":"+currentTime.getMinutes()
+ 				date: this.toTwoDigits(currentTime.getDate())+"/"+
+ 					this.toTwoDigits(currentTime.getMonth())+"/"+
+ 					currentTime.getFullYear(),
+ 				time: this.toTwoDigits(currentTime.getHours()+":"+
+ 					this.toTwoDigits(currentTime.getMinutes())
  			};
+ 		},
+ 		toTwoDigits: function(value) {
+ 			var valueString = (value+1).toString();
+ 			if(valueString.length == 1) {
+ 				valueString = "0"+valueString;
+ 			}
+ 			return valueString;
  		}
  	};
  });
