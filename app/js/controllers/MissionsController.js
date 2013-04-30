202989c97a498104f76e3bf6ba2b7156143a713e
Olivier SCHNEIDER <76267@supinfo.com>
2013-04-30 10:58:47 +0200

    Replace the javascript modal by a javascript confirm dialog

--- app/js/controllers/MissionsController.js
+++ app/js/controllers/MissionsController.js
@@ -1,57 +1,26 @@
 
 smurAngular.controller('MissionsController', 
-	function MissionsController($scope, Mission, $location, $modal, $rootScope, $timeout){
+	function MissionsController($scope, Mission, $location, $window, $rootScope, $timeout){
 		Mission.getAll().then(function(data){
 			$scope.missions = data;
 		});
 
-		$scope.delete = function(id) {
-			Mission.remove(id).then(function(){
-				Mission.getAll().then(function(data){
-					$scope.missions = data;
-				});
-			});
-			$scope.dismiss();
-		};
-
 		$scope.navigateTo = function(mission) {
 			$location.url("/mission/"+mission.id).search({page: "mission"});
-			/*
-			$scope.password = 1234;
-			if($scope.password == "1234") {
-				$scope.dismiss();
-				$location.url("/mission/"+mission.id).search({page: "mission"});
-			} else {
-				$scope.alerts.push({
-					"type": "error",
-					"title": "Mauvais mot de passe",
-					"content": "Essayez 1234"
-				});
-			}*/
 		};
 
 		$scope.goToNewMission = function(){
 			$location.url('/mission/new');
 		};
 
-		$scope.passwordModal = function(mission) {
-			$scope.selectedMission = mission
-			$scope.alerts = [];
-			return $modal({
-				scope: $scope,
-				template: '/partials/missions/password.html', 
-				show: true, 
-				backdrop: 'static'
-			});
-		};
-
 		$scope.deleteModal = function(id) {
-			$scope.element = id;
-			return $modal({
-				scope: $scope,
-				template: '/partials/misc/deleteConfirmation.html', 
-				show: true, 
-				backdrop: 'static'
-			});
+			var confirm = $window.confirm("vous sûr de vouloir supprimer la mission #"+id);
+			if(confirm) {
+				Mission.remove(id).then(function(){
+					Mission.getAll().then(function(data){
+						$scope.missions = data;
+					});
+				});
+			}
 		};
 	});
