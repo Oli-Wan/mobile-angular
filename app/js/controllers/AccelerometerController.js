f5213e94e6eb5f7d69688355804d0d0014203f35
Olivier SCHNEIDER <76267@supinfo.com>
2013-05-10 11:55:43 +0200

    Remove console.log

--- app/js/controllers/AccelerometerController.js
+++ app/js/controllers/AccelerometerController.js
@@ -1,6 +1,5 @@
 smurAngular.controller('AccelerometerController', function($scope, $rootScope){
 	$rootScope.$watch('orientationData', function(newVal, oldVal){
-		console.log("Orientation watching");
 		$scope.orientationData = newVal.originalEvent;
 	});
 });
