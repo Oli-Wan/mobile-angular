91dada4a39cb989650210644fc1abe049fe1cc02
Olivier SCHNEIDER <76267@supinfo.com>
2013-05-13 11:20:35 +0200

    Remove the image from IDB as it has performance issues on mobile and add a loading image

--- app/js/controllers/UpdateMissionController.js
+++ app/js/controllers/UpdateMissionController.js
@@ -1,20 +1,17 @@
-smurAngular.controller("UpdateMissionController", function UpdateMissionController($scope, $window, $routeParams, Mission) {
-	if($scope.mission === undefined) {
-		console.log("loading");
-		Mission.get(parseInt($routeParams.missionId)).then(function(data){
-			$scope.mission = data;
-		});
-	}
-
-	$scope.save = function() {
-		Mission.save($scope.mission).then(function() {
-			$scope.alerts = [];
-			$scope.alerts.push({
-				type: "success",
-				title: "Succès",
-				content: "Mission mise à jour avec succès"
+smurAngular.controller("UpdateMissionController", 
+	function UpdateMissionController($scope, $window, $routeParams, Mission) {
+		
+		$scope.save = function() {
+			//$scope.mission.hiddenImage = $scope.mission.image;
+			$scope.mission.image = undefined;
+			Mission.save($scope.mission).then(function() {
+				$scope.alerts = [];
+				$scope.alerts.push({
+					type: "success",
+					title: "Succès",
+					content: "Mission mise à jour avec succès"
+				});
+				$window.scrollTo(0,0);
 			});
-			$window.scrollTo(0,0);
-		});
-	};
-});
+		};
+	});
