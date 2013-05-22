9eb3f807a91bdf57e15a1fbf53023f0ff8a739d9
Olivier SCHNEIDER <76267@supinfo.com>
2013-05-22 15:44:37 +0200

    Add a threshold to the custom drag directive

--- app/js/directives/ngDrag.js
+++ app/js/directives/ngDrag.js
@@ -1,8 +1,14 @@
 smurAngular.directive('ngDrag', function() {
 	return {
 		restrict: 'A',
+		scope: {
+			threshold: '@ngDragThreshold'
+		},
 		link: function ($scope, element, attrs) {
-			console.log(element);
+			//var threshold = attrs['ng-drag-threshold'];
+
+			var threshold = $scope.threshold;
+			var thresholdExceeded = false;
 
 			Hammer(element[0]).on('dragstart', function(event){
 				$(this).removeClass('animated-return');
@@ -10,17 +16,30 @@ smurAngular.directive('ngDrag', function() {
 
 
 			Hammer(element[0]).on('drag', function(event){
+				var deltaX = event.gesture.deltaX;
+				if(thresholdExceeded)
+					deltaX = deltaX+500;
+
 				$(this).css({
 					position: "relative",
-					left: event.gesture.deltaX+"px"
+					left: deltaX+"px"
 				});
 			});
 
 			Hammer(element[0]).on('dragend', function(event){
 				$this = $(this);
 				$this.addClass('animated-return');
-				$this.css("left","0px");
+				var left = 0;
+				console.log($this.position().left);
+				if($this.position().left > threshold) {
+					left = threshold;
+					thresholdExceeded = true;
+				} else if (thresholdExceeded)
+					thresholdExceeded = false;
+
+
+				$this.css("left",left+"px");
 			});
 		}
 	};
-});
+});	
