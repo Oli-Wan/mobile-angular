3f2fc5a2e0db97842d14c59dca961aa80eae57d9
Olivier SCHNEIDER <76267@supinfo.com>
2013-05-27 11:26:39 +0200

    Add bootstraping

--- app/js/controllers/BootstrapController.js
+++ app/js/controllers/BootstrapController.js
@@ -2,16 +2,20 @@ smurAngular.controller('BootstrapController',
 	function BootstrapController($scope, $http, ClientID, Backend, localStorage, Vehicle, Staff, $window) {
 		var boostrapedKey = "SMUR_BOOSTRAPED";
 		$scope.boostraped = localStorage.getItem(boostrapedKey);
+		$scope.progress = 0;
+
+		$scope.$watch('progress', function(newValue){
+			if(newValue >= 100)
+				$scope.ready = true;
+		});
 
 		$scope.save = function(){
-			console.log($scope.clientID, $scope.backend)
+			$scope.alerts = [];
 			ClientID.set($scope.clientID);
 			Backend.set($scope.backend);
-			localStorage.setItem(boostrapedKey, "true");
 
-			$window.location.reload();
-
-			$http.get($scope.backend+'/persons').success(function(data){
+			var persons = $http.get($scope.backend+'/persons').
+			success(function(data){
 				var count = 0;
 				Staff.clear().then(function(){
 					var recursivePut = function(count, data){
@@ -28,9 +32,17 @@ smurAngular.controller('BootstrapController',
 							count++;
 							if(count < data.length)
 								recursivePut(count, data);
+							else
+								$scope.progress += 50;
 						});
 					};
 					recursivePut(count, data);
+				});
+			}).
+			error(function(){
+				$scope.alerts.push({
+					"type": "error",
+					"title": "Impossible de récupérer les données. Vérifiez que l'adresse du serveur est bonne et qu'il est lancé"
 				});
 			});
 
@@ -48,10 +60,23 @@ smurAngular.controller('BootstrapController',
 							count++;
 							if(count < data.length)
 								recursivePut(count, data);
+							else
+								$scope.progress += 50;
 						});
 					};
 					recursivePut(count, data);
 				});
+			}).
+			error(function(){
+				$scope.alerts.push({
+					"type": "error",
+					"title": "Impossible de récupérer les données. Vérifiez que l'adresse du serveur est bonne et qu'il est lancé"
+				});
 			});
+		};
+
+		$scope.continue = function() {
+			localStorage.setItem(boostrapedKey, true);
+			$window.location.reload();
 		};
 	});
