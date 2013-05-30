800cc7487eca42504820fe7ca822476b952effdf
Olivier SCHNEIDER <76267@supinfo.com>
2013-05-30 10:15:36 +0200

    Add a gif file type to manifest builder

--- appcacheGenerator.sh
+++ appcacheGenerator.sh
@@ -5,5 +5,5 @@ manifest=mobileapp.appcache
 printf "CACHE MANIFEST\n#" > $manifest
 date "+%Y-%m-%d %H:%M:%S" >> $manifest
 printf "\nCACHE:\n" >> $manifest
-find . -type "f" -name "*.js" -o -name "*.html" -o -name "*.json" -o -name "*.css" -o -name "*.png" >> $manifest
+find . -type "f" -name "*.js" -o -name "*.html" -o -name "*.json" -o -name "*.css" -o -name "*.png" -o -name "*.gif" >> $manifest
 printf "\nNETWORK:\n*" >> $manifest
