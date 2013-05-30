2ca039885b712f0c8f5cdba7693b252b50f36276
Olivier SCHNEIDER <76267@supinfo.com>
2013-05-30 14:58:27 +0200

    Improve command list

--- app/js/controllers/CommandController.js
+++ app/js/controllers/CommandController.js
@@ -1,6 +1,8 @@
 mobileAngular.controller('CommandController', 
 	function CommandController($scope, Command) {
+		$scope.commands = [];
 		Command.getAllSorted().then(function(data){
+			console.log(data);
 			$scope.commands = data;
 		});
 	});
