e901bbfc191d9afeeb01237d7d4fffaf831f29f0
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2013-06-06 17:35:04 +0200

    Go back to mission list on launch

--- app/js/directives/launch.js
+++ app/js/directives/launch.js
@@ -1,9 +1,10 @@
-angular.module('mobileAngular').directive('launch', function() {
+angular.module('mobileAngular').directive('launch', function($location) {
 	return {
 		restrict: 'A',
 		link: function ($scope, element, attrs) {
 			var button = angular.element(element);
 			element.bind('click', function(){
+				$location.path("/");
 				screenfull.request();
 			});
 		}
