08ae7a6d9c67b2eab3929fe1af8054eb3e9713fa
Olivier SCHNEIDER <oliwan3dfx@gmail.com>
2013-06-27 11:07:29 +0200

    Add some tests to ngDrag directive

--- test/unit/directives/ngDragSpec.js
+++ test/unit/directives/ngDragSpec.js
@@ -4,10 +4,12 @@
 function createDragEvent(eventName, gestureEvent, element) {
     var event = newEvent(eventName);
     event.gesture = gestureEvent;
+    event.gesture.preventDefault = function () {
+    };
     sendEvent(event, element[0]);
 }
 
-function checkTranslation(element, expected) {
+function checkVerticalTranslation(element, expected) {
     var translate = element.css("transform").toString();
     if (translate.search("matrix") != -1) {
         expect(translate).toEqual('matrix(1, 0, 0, 1, 0, ' + expected + ')');
@@ -15,88 +17,158 @@ function checkTranslation(element, expected) {
         expect(translate).toEqual('translate3d(0, ' + expected + 'px, 0)');
     }
 }
+
+function checkHorizontalTranslation(element, expected) {
+    var translate = element.css("transform").toString();
+    if (translate.search("matrix") != -1) {
+        expect(translate).toEqual('matrix(1, 0, 0, 1, ' + expected + ', 0)');
+    } else {
+        expect(translate).toEqual('translate3d(' + expected + 'px, 0, 0)');
+    }
+}
 // tests
 describe("ngDrag directive", function () {
-    var element, scope;
+    describe("vertical, bounded with threshold method and switch to false", function () {
+        var element, scope;
+        beforeEach(inject(function ($rootScope, $compile) {
+            scope = $rootScope;
+            scope.dragSwitch = false;
+            scope.onThreshold = function () {
+            };
 
-    beforeEach(inject(function ($rootScope, $compile) {
-        scope = $rootScope;
-        scope.dragSwitch = false;
-        scope.onThreshold = function () {
-        };
+            spyOn(scope, "onThreshold").andCallThrough();
 
-        spyOn(scope, "onThreshold").andCallThrough();
+            element = angular.element("<div>" +
+                '<ng-drag switch="dragSwitch" ' +
+                '   bound="200"' +
+                '   axis="y"' +
+                '   bounded="true"' +
+                '   on-threshold="onThreshold()">' +
+                '</ng-drag>' +
+                "</div>");
 
-        element = angular.element("<div>" +
-            '<ng-drag switch="dragSwitch" ' +
-            '   bound="200"' +
-            '   axis="y"' +
-            '   bounded="true"' +
-            '   on-threshold="onThreshold()">' +
-            '</ng-drag>' +
-            "</div>");
+            $compile(element)(scope);
+            scope.$digest();
+        }));
 
-        $compile(element)(scope);
-        scope.$digest();
-    }));
+        describe("the draggable element", function () {
+            it("should be draggable to the top", function () {
+                createDragEvent("dragup", {deltaY: 100}, element);
+                checkVerticalTranslation(element, 100);
+            });
 
-    describe("the draggable element", function () {
-        it("should be draggable to the top", function () {
-            createDragEvent("dragup", {deltaY: 100}, element);
-            checkTranslation(element, 100);
+            it("should be draggable to the bottom", function () {
+                createDragEvent("dragdown", {deltaY: -100}, element);
+                checkVerticalTranslation(element, -100);
+            });
+
+            it("should not be draggable to the left", function () {
+                createDragEvent("dragleft", {deltaX: -100}, element);
+                checkVerticalTranslation(element, 0);
+            });
+
+            it("should not be draggable to the right", function () {
+                createDragEvent("dragright", {deltaX: 100}, element);
+                checkVerticalTranslation(element, 0);
+            });
+
+            it("should slide to target if released while moving forward", function () {
+                createDragEvent("dragup", {deltaY: 100}, element);
+                createDragEvent("dragend", {}, element);
+                checkVerticalTranslation(element, 200);
+            });
+
+            it("should slide to origin if released while moving backward", function () {
+                createDragEvent("dragup", {deltaY: 100}, element);
+                createDragEvent("dragdown", {deltaY: -10}, element);
+                createDragEvent("dragend", {}, element);
+                checkVerticalTranslation(element, 0);
+            });
+
+            it("should be bounded", function () {
+                createDragEvent("dragup", {deltaY: 250}, element);
+                checkVerticalTranslation(element, 200);
+            });
         });
 
-        it("should be draggable to the bottom", function () {
-            createDragEvent("dragdown", {deltaY: -100}, element);
-            checkTranslation(element, -100);
-        });
+        describe("draggable state management", function () {
+            it("should toggle the given switch on bound exceeded", function () {
+                createDragEvent("dragup", {deltaY: 200}, element);
+                createDragEvent("dragend", {}, element);
+                expect(scope.dragSwitch).toBe(true);
+            });
 
-        it("should not be draggable to the left", function () {
-            createDragEvent("dragleft", {deltaX: -100}, element);
-            checkTranslation(element, 0);
-        });
+            it("should move if switch is modified from outside", function () {
+                scope.dragSwitch = true;
+                scope.$apply();
+                checkVerticalTranslation(element, 200);
+            });
 
-        it("should not be draggable to the right", function () {
-            createDragEvent("dragright", {deltaX: 100}, element);
-            checkTranslation(element, 0);
-        });
-
-        it("should slide to target if released while moving forward", function () {
-            createDragEvent("dragup", {deltaY: 100}, element);
-            createDragEvent("dragend", {}, element);
-            checkTranslation(element, 200);
-        });
-
-        it("should slide to origin if released while moving backward", function () {
-            createDragEvent("dragup", {deltaY: 100}, element);
-            createDragEvent("dragdown", {deltaY: -10}, element);
-            createDragEvent("dragend", {}, element);
-            checkTranslation(element, 0);
-        });
-
-        it("should be bounded", function () {
-            createDragEvent("dragup", {deltaY: 250}, element);
-            checkTranslation(element, 200);
+            it("should call callback on threshold", function () {
+                createDragEvent("dragup", {deltaY: 200}, element);
+                createDragEvent("dragend", {}, element);
+                expect(scope.onThreshold).toHaveBeenCalled();
+            });
         });
     });
 
-    describe("draggable state management", function () {
-        it("should toggle the given switch on bound exceeded", function () {
-            createDragEvent("dragup", {deltaY: 200}, element);
-            createDragEvent("dragend", {}, element);
-            expect(scope.dragSwitch).toBe(true);
-        });
-
-        it("should move if switch is modified from outside", function () {
+    describe("horizontal, not bounded, witch is true", function () {
+        var element, scope;
+        beforeEach(inject(function ($rootScope, $compile) {
+            scope = $rootScope;
             scope.dragSwitch = true;
-            scope.$apply();
-            checkTranslation(element, 200);
+
+            element = angular.element("<div>" +
+                '<ng-drag switch="dragSwitch" ' +
+                '   bound="200">' +
+                '</ng-drag>' +
+                "</div>");
+
+            $compile(element)(scope);
+            scope.$digest();
+        }));
+
+        it("should already be translated", function () {
+            checkHorizontalTranslation(element, 200);
         });
 
-        it("should call callback on threshold", function () {
-            createDragEvent("dragup", {deltaY: 200}, element);
+        it("should not be draggable to the top", function () {
+            createDragEvent("dragup", {deltaY: 100}, element);
+            checkHorizontalTranslation(element, 200);
+        });
+
+        it("should not be draggable to the bottom", function () {
+            createDragEvent("dragdown", {deltaY: -100}, element);
+            checkHorizontalTranslation(element, 200);
+        });
+
+        it("should be draggable to the left", function () {
+            createDragEvent("dragleft", {deltaX: -100}, element);
+            checkHorizontalTranslation(element, 100);
+        });
+
+        it("should be draggable to the right and therefore not be bounded", function () {
+            createDragEvent("dragright", {deltaX: 100}, element);
+            checkHorizontalTranslation(element, 300);
+        });
+
+        it("should go back to the bound when dragged further", function () {
+            createDragEvent("dragright", {deltaX: 100}, element);
             createDragEvent("dragend", {}, element);
-            expect(scope.onThreshold).toHaveBeenCalled();
+            checkHorizontalTranslation(element, 200);
+        });
+
+        it("should slide to target if released while moving backward", function () {
+            createDragEvent("dragright", {deltaX: -100}, element);
+            createDragEvent("dragend", {}, element);
+            checkHorizontalTranslation(element, 0);
+        });
+
+        it("should slide to origin if released while moving forward", function () {
+            createDragEvent("dragright", {deltaX: -100}, element);
+            createDragEvent("dragright", {deltaX: 10}, element);
+            createDragEvent("dragend", {}, element);
+            checkHorizontalTranslation(element, 200);
         });
     });
 });
